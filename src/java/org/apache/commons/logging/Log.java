6faf1a65879b30a1e7bec38baa580f43014f8af7
Robert Burrell Donkin <rdonkin@apache.org>
2002-01-03 18:54:29 +0000

    Same interface but added long license and java doc comments
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/logging/trunk@138823 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/logging/Log.java
+++ src/java/org/apache/commons/logging/Log.java
@@ -1,38 +1,89 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//logging/src/java/org/apache/commons/logging/Log.java,v 1.8 2002/01/03 18:54:29 rdonkin Exp $
+ * $Revision: 1.8 $
+ * $Date: 2002/01/03 18:54:29 $
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE file.
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
  */
+
 
 package org.apache.commons.logging;
 
 /**
- * A simple logging interface abstracting logging APIs.  In order to be
+ * <p> A simple logging interface abstracting logging APIs.  In order to be
  * instantiated successfully by {@link LogSource}, classes that implement
  * this interface must have a constructor that takes a single String
- * parameter representing the "name" of this Log.
+ * parameter representing the "name" of this Log. </p>
+ *
+ * <p> The log level determines whether a particular message
+ * should be passed to the logging implementation.
+ * Log levels are ordered numerically.
+ * For example, if the log level is <code>warn</code> 
+ * then the message passed to {@link #error} will be passed to the logging 
+ * implementation but if the log level is <code>fatal</code> or higher
+ * then the message will not.
  *
  * @author Rod Waldhoff
- * @version $Id: Log.java,v 1.7 2001/12/04 04:41:00 craigmcc Exp $
+ * @version $Id: Log.java,v 1.8 2002/01/03 18:54:29 rdonkin Exp $
  */
 public interface Log {
-    public void debug(Object message);
-    public void debug(Object message, Throwable t);
-    public void info(Object message);
-    public void info(Object message, Throwable t);
-    public void warn(Object message);
-    public void warn(Object message, Throwable t);
-    public void error(Object message);
-    public void error(Object message, Throwable t);
-    public void fatal(Object message);
-    public void fatal(Object message, Throwable t);
-    public boolean isDebugEnabled();
-    public boolean isInfoEnabled();
-    public void setLevel(int level);
-    public int getLevel();
 
+
+    // --------------------------------------------------------- Log Level Constants
+    
     /** All logging level. */
     public static final int ALL  = Integer.MIN_VALUE;
     /** "Debug" level logging. */
@@ -47,4 +98,97 @@ public interface Log {
     public static final int FATAL  = 50000;
     /** No logging level. */
     public static final int OFF  = Integer.MAX_VALUE;
+    
+    
+    // --------------------------------------------------------- Logging Properties
+    
+    /**
+     * <p> Is debug logging currently enabled? </p>
+     *
+     * <p> Call this method to prevent having to perform expensive operations
+     * (for example, <code>String</code> concatination)
+     * when the log level is more than debug. </p> 
+     */
+    public boolean isDebugEnabled();
+    
+    /**
+     * <p> Is info logging currently enabled? </p>
+     *
+     * <p> Call this method to prevent having to perform expensive operations
+     * (for example, <code>String</code> concatination)
+     * when the log level is more than debug. </p> 
+     */
+    public boolean isInfoEnabled();
+
+    
+    /**
+     * <p> Set logging level. </p> 
+     *
+     * @param level new logging level
+     */
+    public void setLevel(int level);
+    
+    /**
+     * <p> Get logging level. </p> 
+     */
+    public int getLevel();
+
+    
+
+    // --------------------------------------------------------- Logging Methods
+
+    /**
+     * <p> Log a message with debug log level </p> 
+     */
+    public void debug(Object message);
+    
+    /**
+     * <p> Log an error with debug log level </p> 
+     */
+    public void debug(Object message, Throwable t);
+
+    
+    /**
+     * <p> Log a message with info log level </p> 
+     */
+    public void info(Object message);
+    
+    /**
+     * <p> Log an error with info log level </p> 
+     */
+    public void info(Object message, Throwable t);
+    
+    
+    /**
+     * <p> Log a message with warn log level </p> 
+     */
+    public void warn(Object message);
+
+    /**
+     * <p> Log an error with warn log level </p> 
+     */
+    public void warn(Object message, Throwable t);
+    
+    
+    /**
+     * <p> Log a message with error log level </p> 
+     */
+    public void error(Object message);
+
+    /**
+     * <p> Log an error with error log level </p> 
+     */
+    public void error(Object message, Throwable t);
+    
+    
+    /**
+     * <p> Log a message with fatal log level </p> 
+     */
+    public void fatal(Object message);
+
+    /**
+     * <p> Log an error with fatal log level </p> 
+     */
+    public void fatal(Object message, Throwable t);
+
 }
