b6738eb26e864586f8399db0f41a32cb9eaa0d5b
Simon Kitching <skitching@apache.org>
2006-07-19 23:26:49 +0000

    Add more unit tests for SecurityManager/AccessController issues
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/commons/proper/logging/trunk@423653 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/logging/security/MockSecurityManager.java
+++ src/test/org/apache/commons/logging/security/MockSecurityManager.java
@@ -18,68 +18,118 @@ package org.apache.commons.logging.security;
 
 import java.io.FilePermission;
 import java.security.Permission;
-import java.util.PropertyPermission;
+import java.security.Permissions;
 
 
 /**
  * Custom implementation of a security manager, so we can control the
  * security environment for tests in this package.
- * <p>
- * Note that we don't want to refuse permission to any junit method; otherwise
- * any call to an assert will not be able to output its data!  
  */
 public class MockSecurityManager extends SecurityManager {
+
+    private Permissions permissions = new Permissions();
+    private static final Permission setSecurityManagerPerm =
+        new RuntimePermission("setSecurityManager");
+    
+    private int untrustedCodeCount = 0;
+
+    public MockSecurityManager() {
+        permissions.add(setSecurityManagerPerm);
+    }
+
+    /**
+     * Define the set of permissions to be granted to classes in the o.a.c.l package,
+     * but NOT to unit-test classes in o.a.c.l.security package.
+     */
+    public void addPermission(Permission p) {
+        permissions.add(p);
+    }
+
+    /**
+     * This returns the number of times that a check of a permission failed
+     * due to stack-walking tracing up into untrusted code. Any non-zero
+     * value indicates a bug in JCL, ie a situation where code was not
+     * correctly wrapped in an AccessController block. The result of such a
+     * bug is that signing JCL is not sufficient to allow JCL to perform
+     * the operation; the caller would need to be signed too. 
+     */
+    public int getUntrustedCodeCount() {
+        return untrustedCodeCount;
+    }
+
     public void checkPermission(Permission p) throws SecurityException {
-        // System.out.println("\n\ntesting permission:" + p.getClass() + ":"+ p);
-        
-        // allow read-only access to files, as this is needed to load classes!
-        if (p instanceof FilePermission) {
-            FilePermission fp = (FilePermission) p;
-            if (fp.getActions().equals("read")) {
-                return;
-            }
+        if (setSecurityManagerPerm.implies(p)) {
+            // ok, allow this; we don't want to block any calls to setSecurityManager
+            // otherwise this custom security manager cannot be reset to the original.
+            // System.out.println("setSecurityManager: granted");
+            return;
         }
+
+        // Allow read-only access to files, as this is needed to load classes!
+        // Ideally, we would limit this to just .class and .jar files.
+        if (p instanceof FilePermission) {
+          FilePermission fp = (FilePermission) p;
+          if (fp.getActions().equals("read")) {
+            // System.out.println("Permit read of files");
+            return;
+          }
+        }
+
+        System.out.println("\n\ntesting permission:" + p.getClass() + ":"+ p);
 
         Exception e = new Exception();
         e.fillInStackTrace();
         StackTraceElement[] stack = e.getStackTrace();
         
-        boolean isControlled = false;
+        // scan the call stack from most recent to oldest.
         // start at 1 to skip the entry in the stack for this method
         for(int i=1; i<stack.length; ++i) {
-            String mname = stack[i].getMethodName();
-            if (mname.equals("setSecurityManager")) {
-                // ok, allow this; we don't want to block any calls to setSecurityManager
-                // otherwise this custom security manager cannot be reset to the original
-                // one...
-                // System.out.println("Allow setSecurityManager");
-                return;
-            }
-
             String cname = stack[i].getClassName();
-            //System.out.println("" + i + ":" + stack[i].getClassName() + 
-            //  "." + stack[i].getMethodName());
-            if (cname.startsWith("org.apache.commons.logging")) {
-                isControlled = true;
-                break;
+            System.out.println("" + i + ":" + stack[i].getClassName() + 
+              "." + stack[i].getMethodName());
+
+            if (cname.equals("java.security.AccessController")) {
+                // Presumably method name equals "doPrivileged"
+                //
+                // The previous iteration of this loop verified that the
+                // PrivilegedAction.run method associated with this
+                // doPrivileged method call had the right permissions,
+                // so we just return here. Effectively, the method invoking
+                // doPrivileged asserted that it checked the input params
+                // and found them safe, and that code is trusted, so we
+                // don't need to check the trust level of code higher in
+                // the call stack.
+                System.out.println("Access controller found: returning");
+                return;
+            } else if (cname.startsWith("java.") 
+                || cname.startsWith("javax.") 
+                || cname.startsWith("junit.") 
+                || cname.startsWith("org.apache.tools.ant.")
+                || cname.startsWith("sun.")) {
+                // Code in these packages is trusted if the caller is trusted.
+                //
+                // TODO: maybe check class is loaded via system loader or similar rather
+                // than checking name? Trusted domains may be different in alternative
+                // jvms..
+            } else if (cname.startsWith("org.apache.commons.logging.security")) {
+                // this is the unit test code; treat this like an untrusted client
+                // app that is using JCL
+                ++untrustedCodeCount;
+                System.out.println("Untrusted code [testcase] found");
+                throw new SecurityException("Untrusted code [testcase] found");
+            } else if (cname.startsWith("org.apache.commons.logging.")) {
+                if (permissions.implies(p)) {
+                    // Code here is trusted if the caller is trusted
+                    System.out.println("Permission in allowed set for JCL class");
+                } else {
+                    System.out.println("Permission refused:" + p.getClass() + ":" + p);
+                    throw new SecurityException("Permission refused:" + p.getClass() + ":" + p);
+                }
+            } else {
+                // we found some code that is not trusted to perform this operation.
+                System.out.println("Unexpected code: permission refused:" + p.getClass() + ":" + p);
+                throw new SecurityException("Unexpected code: permission refused:" + p.getClass() + ":" + p);
             }
         }
-        
-        if (!isControlled) {
-            // we have scanned the entire stack, and found no logging classes, so
-            // this must have been called from junit
-            // System.out.println("Not relevant to test; returning success");
-            return;
-        }
-        
-        if (p instanceof PropertyPermission) {
-            // emulate an applet environment where system properties are not accessable
-            throw new SecurityException(
-               "Permission refused to access property:" 
-                    + ((PropertyPermission)p).getName());
-        }
-
-        // emulate an environment where *everything* is refused
-        throw new SecurityException("Permission refused:" + p.getClass() + ":" + p);
     }
 }
